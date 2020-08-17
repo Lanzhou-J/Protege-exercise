42b47249f26dfe071c22f07b1790a4bab12d9cf4
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-17 16:24:58 +1000

    change kata-4 into a function to reuse it in kata-6

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,31 +10,50 @@ namespace HelloWorld
         static void Main(string[] args)
         {
           // // basic-kata-4
+          int Sum(int number){
+            int flagnum = 1;
+            int sum = 0;
+            while(flagnum <= number){
+              sum += flagnum;
+              flagnum++;
+            }
+            // Console.WriteLine(sum);
+            return sum;
+          }
           // Console.WriteLine("Please input a number: ");
           // int number = int.Parse(Console.ReadLine());
-          // Console.WriteLine(number);
-          // int flagnum = 1;
-          // int sum = 0;
-          // while(flagnum <= number){
-          //   sum += flagnum;
-          //   flagnum++;
+          // Console.WriteLine(Sum(number));
+
+
+          // // basic-kata-5
+          // Console.WriteLine("Please input a number: ");
+          // int number = int.Parse(Console.ReadLine());
+          // int startNum = 1;
+          // int sum = 0; 
+          // while(startNum <= number){
+          //   if(startNum % 3 == 0 | startNum % 5 == 0){
+          //     sum += startNum;
+          //   }
+          //   startNum++;
           // }
           // Console.WriteLine(sum);
 
-          // basic-kata-5
-          // List <int> numList = new List <int>();
-          Console.WriteLine("Please input a number: ");
-          int number = int.Parse(Console.ReadLine());
-          // Console.WriteLine(number);
-          int startNum = 1;
-          int sum = 0; 
-          while(startNum <= number){
-            if(startNum % 3 == 0 | startNum % 5 == 0){
-              sum += startNum;
-            }
-            startNum++;
-          }
-          Console.WriteLine(sum);
+          // basic-kata-6
+            Console.WriteLine("Please input a number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nSelect 1 to computing the sum of 1,...,n "); 
+            Console.WriteLine("\nSelect 2 to computing the product of 1,...,n ");
+            Console.WriteLine(">>> ");
+            int selection = int.Parse(Console.ReadLine());      
+            switch (selection)
+            {
+              case 1:
+                Console.WriteLine(Sum(number));
+                break;
+              case 2:
+                Console.WriteLine("Case 2");
+                break;
+          }                              
         }
     }
 }
