966bf7922f12a3381e78bbd25e9937c511226683
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-17 13:34:51 +1000

    finished basic coding katas 1/2/3/4

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,16 +9,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-          Console.WriteLine("Hello World");
-          Console.WriteLine("\nWhat is your name? ");
-          string name = Console.ReadLine();
-          DateTime date = DateTime.Now;
-          if(name=="Alice"||name=="Bob")
-          {
-          Console.WriteLine($"\nHello, {name}, on {date:D} at {date:t}!");
+          Console.WriteLine("Please input a number: ");
+          int number = int.Parse(Console.ReadLine());
+          Console.WriteLine(number);
+          int flagnum = 1;
+          int sum = 0;
+          while(flagnum <= number){
+            sum += flagnum;
+            flagnum++;
           }
-          Console.Write("\nPress any key to exit...");
-          Console.ReadKey(false);
+          Console.WriteLine(sum);
         }
     }
 }
