c8966a08b536fb9d155ce25508a5804864e51c88
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-17 16:49:22 +1000

    Basic-kata-7 is solved.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,16 +10,16 @@ namespace HelloWorld
         static void Main(string[] args)
         {
           // // basic-kata-4
-          int Sum(int number){
-            int flagnum = 1;
-            int sum = 0;
-            while(flagnum <= number){
-              sum += flagnum;
-              flagnum++;
-            }
-            // Console.WriteLine(sum);
-            return sum;
-          }
+          // int Sum(int number){
+          //   int flagnum = 1;
+          //   int sum = 0;
+          //   while(flagnum <= number){
+          //     sum += flagnum;
+          //     flagnum++;
+          //   }
+          //   // Console.WriteLine(sum);
+          //   return sum;
+          // }
           // Console.WriteLine("Please input a number: ");
           // int number = int.Parse(Console.ReadLine());
           // Console.WriteLine(Sum(number));
@@ -38,34 +38,44 @@ namespace HelloWorld
           // }
           // Console.WriteLine(sum);
 
-          // basic-kata-6
-            Console.WriteLine("Please input a number: ");
-            int number = int.Parse(Console.ReadLine());
-            Console.WriteLine("\nSelect 1 to computing the sum of 1,...,n "); 
-            Console.WriteLine("\nSelect 2 to computing the product of 1,...,n ");
-            Console.WriteLine(">>> ");
-            int selection = int.Parse(Console.ReadLine());
 
-            //product function
-            int Product(int number){
-              int startNum = 1;
-              int product = 1;
-              while(startNum <= number){
-                product *= startNum;
-                startNum++;              
-              }
-              return product;
+          // // basic-kata-6
+          //   Console.WriteLine("Please input a number: ");
+          //   int number = int.Parse(Console.ReadLine());
+          //   Console.WriteLine("\nSelect 1 to computing the sum of 1,...,n "); 
+          //   Console.WriteLine("\nSelect 2 to computing the product of 1,...,n ");
+          //   Console.WriteLine(">>> ");
+          //   int selection = int.Parse(Console.ReadLine());
+          //   //product function
+          //   int Product(int number){
+          //     int startNum = 1;
+          //     int product = 1;
+          //     while(startNum <= number){
+          //       product *= startNum;
+          //       startNum++;              
+          //     }
+          //     return product;
+          //   }
+
+          //   switch (selection)
+          //   {
+          //     case 1:
+          //       Console.WriteLine(Sum(number));
+          //       break;
+          //     case 2:
+          //       Console.WriteLine(Product(number));
+          //       break;
+          // }
+
+
+          // basic-kata-7
+          for(int i = 1; i <= 12; i++){
+            List <int> list = new List <int>(); 
+            for(int j = 1; j <= 12; j++){
+              list.Add(i*j);
             }
-
-            switch (selection)
-            {
-              case 1:
-                Console.WriteLine(Sum(number));
-                break;
-              case 2:
-                Console.WriteLine(Product(number));
-                break;
-          }                              
+            Console.WriteLine(string.Join(", ", list));              
+          }                             
         }
     }
 }
