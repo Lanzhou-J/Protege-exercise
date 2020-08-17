cbd9a973ebe3ea859b8cceee8e56425e046e0d4a
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-17 15:54:11 +1000

    Basic coding katas 5 solved.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-// using System.Collections.Generic;
-// using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
 // using System.Text;
 
 namespace HelloWorld
@@ -9,14 +9,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+          // // basic-kata-4
+          // Console.WriteLine("Please input a number: ");
+          // int number = int.Parse(Console.ReadLine());
+          // Console.WriteLine(number);
+          // int flagnum = 1;
+          // int sum = 0;
+          // while(flagnum <= number){
+          //   sum += flagnum;
+          //   flagnum++;
+          // }
+          // Console.WriteLine(sum);
+
+          // basic-kata-5
+          // List <int> numList = new List <int>();
           Console.WriteLine("Please input a number: ");
           int number = int.Parse(Console.ReadLine());
-          Console.WriteLine(number);
-          int flagnum = 1;
-          int sum = 0;
-          while(flagnum <= number){
-            sum += flagnum;
-            flagnum++;
+          // Console.WriteLine(number);
+          int startNum = 1;
+          int sum = 0; 
+          while(startNum <= number){
+            if(startNum % 3==0 | startNum % 5==0){
+              sum += startNum;
+            }
+            startNum++;
           }
           Console.WriteLine(sum);
         }
