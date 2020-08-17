26a4049cf496561356d49b01f26b2bc07d40de25
Lanzhou Jiang <lzjiang99@gmail.com>
2020-08-17 12:15:08 +1000

    first exercise

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Hello World");
+          Console.WriteLine("\nWhat is your name? ");
+          string name = Console.ReadLine();
+          DateTime date = DateTime.Now;
+          if(name=="Alice"||name=="Bob")
+          {
+          Console.WriteLine($"\nHello, {name}, on {date:D} at {date:t}!");
+          }
+          Console.Write("\nPress any key to exit...");
+          Console.ReadKey(false);
+        }
+    }
+}
